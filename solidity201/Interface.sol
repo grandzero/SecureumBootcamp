a14dec12d1cead67b4fcd821915ec7bb655a1046
grandzero <bayramutkuuzunlar@gmail.com>
2022-02-12 14:59:31 +0300

    add oop approach and gas usage to interface contract

--- solidity201/Interface.sol
+++ solidity201/Interface.sol
@@ -69,3 +69,56 @@ contract C2_Loader{
         shildAddress.setB(25); // This reverts tx
     }
 }
+
+
+/*
+    OOP Best Practice : Always try to write code agains interface rather then object(in our case it's contract) 
+*/
+
+contract ChildBase is Child{
+    uint256 public test;
+
+    function setA(uint256 _a) 
+    override // Throws an error, if this keyword is not included !!
+    external{
+        test=_a;
+    }
+    function setB(uint256 _a) 
+    override // Throws an error, if this keyword is not included !!
+    external{
+        test=_a;
+    }
+}
+
+contract C3_InterfaceCaller{
+    IBase public baseInterface;
+    Child public childInterface;
+    ChildBase public base;
+    constructor(address _cAddr){
+        baseInterface = IBase(_cAddr);
+        childInterface = Child(_cAddr);
+        base = new ChildBase();
+    }
+
+    function callFromBaseI(IBase addr) public{
+        // Gas: 27653
+        addr.setA(2);
+    }
+
+    function callFromChild(Child addr) public{
+        // Gas: 30432
+        addr.setA(3);
+    }
+
+    function callFromContract(ChildBase addr) public{
+        // Gas: 30388
+        addr.setA(4);
+    }
+
+    function callFunction() external {
+        // Gas: 51356
+        callFromBaseI(base);
+        callFromChild(base);
+        callFromContract(base);
+    }
+}
